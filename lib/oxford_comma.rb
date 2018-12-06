90552b5d2f618d079742a531ed1d7d5d8093cc6a
Daniel Fries <daniel.w.fries@gmail.com>
2018-12-06 19:33:00 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  output = ""
+  if array.length == 1
+    output = array.join
+  elsif array.length == 2
+    output = array[0] + " and " + array[1]
+  else
+    last = array.pop
+    output = array.join(", ") + ", and " + last
+  end
+return output
 end
